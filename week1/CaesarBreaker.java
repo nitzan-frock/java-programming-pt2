9c6b8e83482bff23852d67e4c869d3259ac55030
Nitzan Frock <nitzanfrock@gmail.com>
2019-03-12 15:28:00 -0400

    change file structure to week based

---
+++ week1/CaesarBreaker.java
@@ -0,0 +1,14 @@
+import "../assignment3/
+/**
+ * Write a description of CaesarBreaker here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class CaesarBreaker {
+    public int countLetters() {}
+    
+    public int maxIndex() {}
+    
+    public String decrypt() {}
+}

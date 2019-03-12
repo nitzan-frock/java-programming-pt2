53a7e0841124d003df0ad6f2d7afa2b268ed0903
Nitzan Frock <nitzanfrock@gmail.com>
2019-03-12 10:38:29 -0400

    completed countWordLengths

--- Assignment3/WordLengths.java
+++ Assignment3/WordLengths.java
@@ -7,6 +7,45 @@ import edu.duke.*;
  */
 public class WordLengths {
     public void countWordLengths(FileResource resource, int[] counts){
+        for (String word : resource.words()) {
+            int nonLetters = 0;
+            int letters = 0;
+            for (int k = 0; k < word.length(); k++){
+                char currChar = word.charAt(k);
+                if (currChar == '-' 
+                    && Character.isLetter(word.charAt(k-1)) 
+                    && Character.isLetter(word.charAt(k+1))){
+                        letters++;
+                } else if (Character.isLetter(currChar)){
+                    letters++;
+                }
+            }
+            
+            if (letters >= 30) counts[30] += 1;
+            else counts[letters] += 1;
+        }
         
+        for (int k = 1; k < counts.length; k++) {
+            System.out.println(counts[k] + " words of length " + k);
+        }
+    }
+    
+    public int indexOfMax(int[] values) {
+        int index = 0;
+        int max = 0;
+        for (int k = 0; k < values.length; k++) {
+            if (values[k] > max) {
+                max = values[k];
+                index = k;
+            }
+        }
+        return index;
+    }
+
+    public void testCountWordLengths() {
+        FileResource fr = new FileResource("data/data.txt");
+        int[] counts = new int[31];
+        countWordLengths(fr, counts);
+        System.out.println("Most common length of word: " + indexOfMax(counts));
     }
 }

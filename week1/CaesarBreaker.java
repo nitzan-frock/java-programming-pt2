07acc1f123ae49918c1877315114bb403d6059a5
Nitzan Frock <nitzanfrock@gmail.com>
2019-03-12 16:08:37 -0400

    working on decrypt method

--- week1/CaesarBreaker.java
+++ week1/CaesarBreaker.java
@@ -1,4 +1,3 @@
-import "../assignment3/
 /**
  * Write a description of CaesarBreaker here.
  * 
@@ -6,9 +5,53 @@ import "../assignment3/
  * @version (a version number or a date)
  */
 public class CaesarBreaker {
-    public int countLetters() {}
+    public int[] countLetters(String message) {
+        int[] counts = new int[26];
+        String alpha = "abcdefghijklmnopqrstuvwxyz";
+        for (int k = 0; k < message.length(); k++) {
+            char c = message.charAt(k);
+            int idx = alpha.indexOf(c);
+            if (idx != -1) counts[idx]++;
+        }
+        return counts;
+    }
     
-    public int maxIndex() {}
+    public int indexOfMax(int[] values) {
+        int index = 0;
+        int max = 0;
+        for (int k = 0; k < values.length; k++) {
+            if (values[k] > max) {
+                max = values[k];
+                index = k;
+            }
+        }
+        return index;
+    }
     
-    public String decrypt() {}
+    public String decrypt(String encrypted) {
+        CaesarCipher cc = new CaesarCipher();
+        int[] freqs = countLetters(encrypted);
+        int commonLetterIdx = indexOfMax(freqs);
+        int decryptKey = commonLetterIdx - 4;
+        if (commonLetterIdx < 4) decryptKey = 26 - (4 - commonLetterIdx);
+        
+        return cc.encrypt(encrypted, 26-decryptKey);
+    }
+    
+    public String halfOfString(String message, int start) {
+        StringBuilder everyOther = new StringBuilder();
+        for (int k = start; k < message.length(); k += 2) {
+            everyOther.append(message.charAt(k));
+        } 
+        return everyOther.toString();
+    }
+    
+    public void testDecrypt() {
+        CaesarCipher cc = new CaesarCipher();
+        String msg = "MY PASSWORD IS YOU SHALL NOT DECRYPT MEEEEEEEEEEEEEEEEEEEEEEEE!";
+        int key = 11;
+        String encrypted = cc.encrypt(msg, 26 - key);
+        String decrypted = decrypt(encrypted);
+        System.out.println(encrypted + "\n" + decrypted);
+    }
 }
